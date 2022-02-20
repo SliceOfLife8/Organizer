82cb750f8823205904ab7fa892674bea7a1594ad
Petimezas, Chris, Vodafone <chris.petimezas@vodafone.com>
2022-02-20 18:55:22 +0200

    Initial Commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Petimezas, Chris, Vodafone on 20/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
