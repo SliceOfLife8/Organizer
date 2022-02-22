9bdb81cb54b265896f98a1e8383d82fda557efef
Petimezas, Chris, Vodafone <chris.petimezas@vodafone.com>
2022-02-22 19:46:14 +0200

    Add .gitignore file

--- Pods/Target Support Files/Pods-Organizer-OrganizerUITests/Pods-Organizer-OrganizerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Organizer_OrganizerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Organizer_OrganizerUITests
-@end
--- Pods/Target Support Files/Pods-OrganizerTests/Pods-OrganizerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OrganizerTests : NSObject
-@end
-@implementation PodsDummy_Pods_OrganizerTests
-@end
